7847c9575d5d314e612354870851e1bee6130677
BiXingyu <3245454323@qq.com>
2023-02-06 17:25:24 +0800

    用代码生成工具进行逆向工程

--- gulimall-member/src/test/java/com/bixingyu/gulimall/member/GulimallMemberApplicationTests.java
+++ gulimall-member/src/test/java/com/bixingyu/gulimall/member/GulimallMemberApplicationTests.java
@@ -1,13 +1,27 @@
 package com.bixingyu.gulimall.member;
 
+import com.bixingyu.gulimall.member.entity.MemberEntity;
+import com.bixingyu.gulimall.member.service.MemberService;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.annotation.Resource;
+@Slf4j
 @SpringBootTest
 class GulimallMemberApplicationTests {
-
+    @Resource
+    private MemberService memberService;
     @Test
     void contextLoads() {
+        MemberEntity memberEntity = new MemberEntity();
+        memberEntity.setUsername("BiXingyu");
+        memberService.save(memberEntity);
+
+        log.info("保存成功{}" , memberEntity);
+
+
+
     }
 
 }
